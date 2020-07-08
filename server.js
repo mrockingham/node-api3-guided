ffea19aa7a592436f8dbe856249b20dbdfe3d221
Michael Rockingham <mrockingham2@gmail.com>
2020-07-08 15:56:52 -0400

    backup

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 const express = require('express'); // importing a CommonJS module
+const morgan =require('morgan')
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+
+
+// the three amigas
+function GateKeeper(req, res, next){
+  const seconds = new Date().getSeconds()
+
+  if(seconds % 3 === 0){
+    res.status(403).json({you: 'cannot pass!'})
+  } else{
+    next()
+  }
+}
+
+function requiresAuth(req, res, next){
+  const {password} = req.headers
+
+  if(password) {
+    next()
+  } else{
+    res.status(401).json({erro: 'wrong password'})
+  }
+}
+
+
+
+
+// Global middleware
+server.use(morgan(':method' ))
 server.use(express.json());
 
 server.use('/api/hubs', hubsRouter);
